16141f24fbfff166dd22151770ae00a0fb778b22
Andrew Murray <andyranted@gmail.com>
2020-07-05 10:49:14 -0400

    Normalise spacing in track.test.js also

--- src/__tests__/track.test.js
+++ src/__tests__/track.test.js
@@ -2,21 +2,21 @@ import track from '../track'
 import fs from "fs"
 
 test('unit test trackFromPositions', () => {
-	const positions = [
-		48,
-		48 * 2,
-		48 * 5
-	];
-	const size = 48 * 6;
-	const exampleTrack = track.trackFromPositions( positions, size );
-	
-	expect(exampleTrack.resolution).toBe(48);
-	expect(exampleTrack.length()).toBe(size);
-	expect(exampleTrack.rep).toStrictEqual([0,1,1,0,0,1]);
+  const positions = [
+    48,
+    48 * 2,
+    48 * 5
+  ];
+  const size = 48 * 6;
+  const exampleTrack = track.trackFromPositions( positions, size );
+  
+  expect(exampleTrack.resolution).toBe(48);
+  expect(exampleTrack.length()).toBe(size);
+  expect(exampleTrack.rep).toStrictEqual([0,1,1,0,0,1]);
 
-	expect(() => track.trackFromPositions( positions, 0 )).toThrow('size');
-	expect(() => track.trackFromPositions( positions, -5 )).toThrow('size');
-	expect(() => track.trackFromPositions( positions, 20 )).toThrow('size');
-	expect(() => track.trackFromPositions( positions, 48 * 5 )).toThrow('size');
+  expect(() => track.trackFromPositions( positions, 0 )).toThrow('size');
+  expect(() => track.trackFromPositions( positions, -5 )).toThrow('size');
+  expect(() => track.trackFromPositions( positions, 20 )).toThrow('size');
+  expect(() => track.trackFromPositions( positions, 48 * 5 )).toThrow('size');
 });
 
