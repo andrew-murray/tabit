ffebd0eec7e32a2881deda5b489aa10d87dcabe3
Andrew Murray <andyranted@gmail.com>
2020-07-07 20:01:24 -0400

    Beginnings of work on notation formatting, write and test some subordinate functions with mostly complete testing. Needs more testing and to uncomment the overarching function :)

---
+++ src/__tests__/notation.test.js
@@ -0,0 +1,171 @@
+import notation from '../notation'
+import track from '../track'
+
+test('unit test chunkString', () => {
+  const original = "abcdefghijkl";
+
+
+  // invalid cases
+  expect(() => notation.chunkString(original, 0)).toThrow();
+  expect(() => notation.chunkString(original, -1)).toThrow();
+
+  // >= length
+  expect(notation.chunkString(original,15)).toEqual([original]);
+  // == length
+  expect(notation.chunkString(original,12)).toEqual([original]);
+
+  expect(notation.chunkString(original,10)).toEqual([
+    "abcdefghij",
+    "kl"
+  ]);
+
+  expect(notation.chunkString(original,6)).toEqual([
+    "abcdef",
+    "ghijkl"
+  ]);
+
+  expect(notation.chunkString(original,4)).toEqual([
+    "abcd",
+    "efgh",
+    "ijkl"
+  ]);
+
+  expect(notation.chunkString(original,3)).toEqual([
+    "abc",
+    "def",
+    "ghi",
+    "jkl"
+  ]);
+
+  expect(notation.chunkString(original,1)).toEqual(
+    original.split("")
+  );
+
+});
+
+test('unit test createNumberMarker', () => {
+
+  const configExample = {
+    "restMark" : "-",
+    "beatMark" : "|",
+    "lineMark" : "|",
+    "numberRestMark" : "-",
+    "beatResolution" : 4, // 48
+    "showBeatMark" : true,
+    "showBeatNumbers" : true,
+    "lineResolution" : 4 * 8 // 48 * 8
+  };
+
+  expect( notation.createNumberMarker( configExample, 1, 4 * 20 ) ).toEqual( "1---2---3---4---5---6---7---8---" );
+
+  expect( notation.createNumberMarker( configExample, 1, 4 * 8 ) ).toEqual( "1---2---3---4---5---6---7---8---" );
+  expect( notation.createNumberMarker( configExample, 1, 4 * 7 ) ).toEqual( "1---2---3---4---5---6---7---" );
+  expect( notation.createNumberMarker( configExample, 1, 4 * 3 ) ).toEqual( "1---2---3---" );
+
+  // non integer
+  expect( notation.createNumberMarker( configExample, 1, 4 * 7 + 2 ) ).toEqual( "1---2---3---4---5---6---7---8-" );
+
+  // all the above checks, with a different patternResolution
+  expect( notation.createNumberMarker( configExample, 2, 4 * 20 ) ).toEqual( "1-2-3-4-5-6-7-8-" );
+
+  expect( notation.createNumberMarker( configExample, 2, 4 * 8 ) ).toEqual( "1-2-3-4-5-6-7-8-" );
+  expect( notation.createNumberMarker( configExample, 2, 4 * 7 ) ).toEqual( "1-2-3-4-5-6-7-" );
+  expect( notation.createNumberMarker( configExample, 2, 4 * 3 ) ).toEqual( "1-2-3-" );
+
+  // non integer
+  // note, this is pretty weird, perhaps 7-- would be better? So this is more of a regression test
+  expect( notation.createNumberMarker( configExample, 2, 4 * 7 + 2 ) ).toEqual( "1-2-3-4-5-6-7-8"  );
+
+});
+
+test('unit test createNumberMarker Realistic', () => {
+
+  const configExample = {
+    "restMark" : "X",
+    "beatMark" : "B",
+    "numberRestMark" : "T",
+    "beatResolution" : 48,
+    "showBeatNumbers" : true,
+    "lineResolution" : 48 * 4
+  };
+
+  expect( notation.createNumberMarker( configExample,  48 / 3, 48 * 3) ).toEqual( "1TT2TT3TT" );
+  expect( notation.createNumberMarker( configExample,  48 / 3, 48 * 2) ).toEqual( "1TT2TT" );
+  expect( notation.createNumberMarker( configExample,  48 / 3, (48 / 3) * 2 ) ).toEqual( "1T" );
+
+  expect( notation.createNumberMarker( configExample,  48 / 3, 48 * 4) ).toEqual( "1TT2TT3TT4TT" );
+  expect( notation.createNumberMarker( configExample,  48 / 3, 48 * 5) ).toEqual( "1TT2TT3TT4TT" );
+
+});
+
+test('unit test createNumberMarker Realistic', () => {
+
+  const configExample = {
+    "restMark" : "X",
+    "beatMark" : "B",
+    "numberRestMark" : "T",
+    "beatResolution" : 48,
+    "showBeatNumbers" : true,
+    "lineResolution" : 48 * 4
+  };
+
+  // repeat a normal case
+
+  expect( notation.createNumberMarker( configExample,  48 / 3, 48 * 3) ).toEqual( "1TT2TT3TT" );
+
+  // beatResolution doesn't divide lineResolution
+  configExample.lineResolution = 49;
+  expect( () => notation.createNumberMarker( configExample,  48 / 3, 48 * 3) ).toThrow();
+
+  configExample.lineResolution = -1;
+  expect( () => notation.createNumberMarker( configExample,  48 / 3, 48 * 3) ).toThrow();
+  configExample.lineResolution = 0;
+  expect( () => notation.createNumberMarker( configExample,  48 / 3, 48 * 3) ).toThrow();
+  configExample.lineResolution = 48 *4;
+
+
+  // patternResolution doesn't divide beatResolution
+  configExample.beatResolution = 48 / 2;
+  expect( () => notation.createNumberMarker( configExample,  48 / 3, 48 * 3) ).toThrow();
+
+
+});
+
+
+test('unit test formatLineWithMarkers', () => {
+  const configExample = {
+    "restMark" : ".",
+    "beatMark" : "|",
+    "lineMark" : " ",
+    "numberRestMark" : "-",
+    "beatResolution" : 48,
+    "showBeatMark" : true,
+    "showBeatNumbers" : true,
+    "lineResolution" : 48 * 4
+  };
+
+  const exampleLine = "O.....O.....O...";
+
+  // change resolution of passed in string
+
+  // this turns out to be very nasty looking notation when you test all the options
+  expect(notation.formatLineWithMarkers(configExample, exampleLine, 24)).toEqual( " O.|..|..|O.|..|..|O.|.. ");
+
+  expect(notation.formatLineWithMarkers(configExample, exampleLine, 12)).toEqual( " O...|..O.|....|O... ");
+  // lineLength / beatResolution
+  // (16 * 16) / 48 = 5.3333
+  expect(notation.formatLineWithMarkers(configExample, exampleLine, 16)).toEqual( " O..|...|O..|...|O..|. ");
+
+  // change beatResolution
+  configExample.beatResolution = 24;
+  expect(notation.formatLineWithMarkers(configExample, exampleLine, 24)).toEqual( " O|.|.|.|.|.|O|.|.|.|.|.|O|.|.|. ");
+
+  configExample.beatResolution = 96;
+  expect(notation.formatLineWithMarkers(configExample, exampleLine, 24)).toEqual( " O...|..O.|....|O... ");
+  configExample.beatResolution = 192;
+  expect(notation.formatLineWithMarkers(configExample, exampleLine, 24)).toEqual( " O.....O.|....O... ");
+
+
+  // FIXME: The testing of formatLineWithMarkers is not as thorough as other functions
+});
+
---
+++ src/notation.js
@@ -0,0 +1,141 @@
+import track from "./track";
+
+class notation
+{
+
+  static DEFAULT_FORMAT_CONFIG = {
+    "restMark" : "-",
+    "beatMark" : "|",
+    "lineMark" : "|",
+    "numberRestMark" : "-",
+    "beatResolution" : 48,
+    "showBeatMark" : true,
+    "showBeatNumbers" : true,
+    "lineResolution" : 48 * 8
+  };
+
+  static validateConfig(config, patternResolution) {
+    if( patternResolution != null && ( config.beatResolution % patternResolution ) != 0 )
+    {
+      throw new Error("patternResolution doesn't divide beatResolution");
+    }
+    if( config.lineResolution <= 0 )
+    {
+      throw new Error("config.lineResolution doesn't divide patternResolution");
+    }
+  }
+
+  static chunkString(str, chunkSize) {
+    if( chunkSize <= 0 )
+    {
+      throw new Error("chunkSize must be > 0")
+    }
+    return str.match(new RegExp('.{1,' + chunkSize + '}', 'g'));
+  }
+
+  // in javascript strings are immutable
+  // various parts of this code, have to be fixed!
+
+  static createNumberMarker(config, patternResolution, patternSize)
+  {
+    if( patternResolution <= 0 || patternSize <= 0 )
+    {
+      throw new Error("(patternResolution, patternSize) arguments <= 0");
+    }
+    if( ( patternSize % patternResolution) != 0 )
+    {
+      throw new Error("patternSize,doesn't divide patternResolution");
+    }
+
+    notation.validateConfig(config, patternResolution);
+
+    // note, this assumes that each line makes this match evenly
+    // but ... there's nothing you can do in that case really
+    const lineLength = Math.min(patternSize, config.lineResolution);
+    let beatCount = Math.ceil(lineLength / config.beatResolution);
+
+    let numberMarkerArray = Array.from( Array(lineLength / patternResolution), e => config.numberRestMark );
+
+    for( let beat = 0; beat < beatCount; beat++ )
+    {
+      numberMarkerArray[ beat * ( config.beatResolution / patternResolution ) ] = ( (beat+1) % 10 ).toString();
+    }
+    return numberMarkerArray.join("");
+  }
+
+  static formatLineWithMarkers(config, line, patternResolution)
+  {
+    notation.validateConfig(config);
+
+    const beatChunkSize = config.beatResolution / patternResolution;
+
+
+    const lineWithBeats = config.showBeatMark ? notation.chunkString(line, beatChunkSize).join(config.beatMark) : line;
+    // note: we choose to always show the lineMarker even if it doesn't match the line resolution
+    return config.lineMark + lineWithBeats + config.lineMark;
+  }
+
+  /*
+  function notationFromInstrumentAndTrack(
+    instrument,
+    trackDict,
+    formatConfig = {}
+  )
+  {
+    for( const [propName, propValue] of Object.entries(formatConfig))
+    {
+      if(!config.hasOwnProperty(propName))
+      {
+        throw new Exception("passed unrecognised property " + propName);
+      }
+    }
+
+    let config = Object.assign( Object.assign({}, DEFAULT_FORMAT_CONFIG), formatConfig );
+
+    let instrumentTracks = Object.values(trackDict);
+    if(instrumentTracks.length == 0)
+    {
+      return "";
+    }
+
+    // turn the tracks, into one char string
+
+    const patternLength = instrumentTracks[0].size;
+    const patternResolution = instrumentTracks[0].resolution;
+    const notationLength = instrumentTracks[0].size / instrumentTracks[0].resolution;
+    // we only format tracks to the correct resolution
+    let patternString = config.restMark.repeat(notationLength);
+    for( let charIndex = 0; charIndex < blankString.length; ++charIndex)
+    {
+      // todo: handle collisions
+      for( const [trackID, trackSymbol] of Object.entries(instrument) )
+      {
+        const track = trackDict[trackID];
+        if( track.rep[charIndex] == 1 )
+        {
+          patternString[charIndex] = trackSymbol;
+        }
+      }
+    }
+
+    // handle lines and beatMarkers
+
+
+    let lineArray = chunkString( patternString, config.lineResolution );
+
+
+    let formattedLineArray = [];
+    // add numbers on the first line
+
+
+
+    for( let i = 0; i < lineArray.length; ++i )
+    {
+      formattedLineArray.push( formatLineWithMarkers( lineArray[i], line));
+    }
+
+  }
+  */
+};
+
+export default notation;
