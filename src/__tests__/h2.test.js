80cdd51262103ac399d5d872351d7a12ce717a14
Andrew Murray <andyranted@gmail.com>
2020-07-09 19:25:51 -0400

    Change and restructure integration test.
    The input test .h2songs have been updated.
    h2 parsing now checks against json on disk, rather than a jest snapshot.
    
    There's now a more formalised integration test of notation generator.
    It reads the committed json output of the h2 parsing and formats a massive snapshot.
    I wonder if there's a facility for me to name these snapshots.
    
    In addition, the structure of the h2 testing has slightly changed to
    ensure potential errors in the async functions are handled correctly.

--- src/__tests__/h2.test.js
+++ src/__tests__/h2.test.js
@@ -4,29 +4,30 @@ import h2 from "../h2"
 import notation from "../notation"
 import fs from "fs"
 
-test('h2 parsing', () => {
+test('h2 parsing garlic', async () => {
   const testXml = fs.readFileSync("./test_data/too_much_garlic.h2song");
-  h2.parseHydrogenPromise(testXml.toString()).then(function(result)
+  const testJSON = "./test_data/too_much_garlic.json";
+  
+  const resultJSONPromise = h2.parseHydrogenPromise(testXml.toString()).then(result =>
   {
-    expect(result).toMatchSnapshot();
-
-    let concatString = "";
-    for( const track of result.patterns )
-    {
-      const name = track.name;
-      concatString += name + "\n\n";
-      for( const [instrumentID, instrumentObj] of Object.entries(result.instruments) )
-      {
-        const instrument = { [instrumentID.toString()]: "X" };
-        const notationString = notation.fromInstrumentAndTrack(
-          instrument,
-          track.instrumentTracks
-          // default config
-        );
-        concatString += instrumentObj.name + "\n\n";
-        concatString += notationString + "\n\n";
-      }
-    }
-    expect(concatString).toMatchSnapshot();
+    return JSON.stringify(result, null, 4);
   });
+  const resultJSON = await resultJSONPromise;
+  // fs.writeFileSync(testJSON, resultJSON);
+  const expectedJSON = String(fs.readFileSync(testJSON));
+  return expect(resultJSON).toEqual(expectedJSON);
+});
+
+test('h2 parsing kuva', async () => {
+  const testXml = fs.readFileSync("./test_data/kivakovakivikuva.h2song");
+  const testJSON = "./test_data/kivakovakivikuva.json";
+  
+  const resultJSONPromise = h2.parseHydrogenPromise(testXml.toString()).then(result =>
+  {
+    return JSON.stringify(result, null, 4);
+  });
+  const resultJSON = await resultJSONPromise;
+  // fs.writeFileSync(testJSON, resultJSON);
+  const expectedJSON = String(fs.readFileSync(testJSON));
+  return expect(resultJSON).toEqual(expectedJSON);
 });
---
+++ src/__tests__/notationIntegration.test.js
@@ -0,0 +1,84 @@
+// notationIntegration.test.js
+
+import fs from "fs"
+
+import notation from "../notation"
+import track from "../track"
+
+function formatAsPage(state, instruments, config = {})
+{
+  let page = "";
+  for( const track of state.patterns )
+  {
+    const name = track.name;
+    page += name + "\n\n";
+    for( const [instrumentName, instrument] of instruments )
+    {
+      const notationString = notation.fromInstrumentAndTrack(
+        instrument,
+        track.instrumentTracks,
+        config
+      );
+      page += instrumentName + "\n\n";
+      page += notationString + "\n\n";
+    }
+  }
+  return page;
+}
+
+function createObjects(state)
+{
+  // the instruments currently work as simple objects
+  // we need to create tracks!
+  for( let pattern of state.patterns )
+  {
+    let replacedTracks = {};
+    // todo: find a more compact way of doing this
+    for( const [id, trackData] of Object.entries(pattern.instrumentTracks) )
+    {
+      replacedTracks[id] = new track( trackData.rep, trackData.resolution );
+    }
+    pattern.instrumentTracks = replacedTracks;
+  }
+  return state;
+}
+
+const configuredInstrumentMappings = [
+  ["Bottom Bass", {"0" : "O", "18" : "X"}],
+  ["Mid Bass", {"1" : "X", "17" : "O"}], 
+  ["Shaker", {"16" : "x", "21" : "X"}],
+  ["Snare", {"2" : "X", "13" : "-"}], 
+  ["Djembe", {"10" : "S", "11" : "t", "12" : "O"}]
+];
+
+test('notation separate garlic', () => {
+  const state = createObjects(JSON.parse(fs.readFileSync("./test_data/too_much_garlic.json")));
+  const instrumentMappings = Array.from(
+    Object.entries(state.instruments),
+    (idAndObj) => [ idAndObj[1].name, { [idAndObj[0].toString()]: "X" } ]
+  );
+  const output = formatAsPage( state, instrumentMappings );
+  expect(output).toMatchSnapshot();
+});
+
+test('notation separate kuva', () => {
+  const state = createObjects(JSON.parse(fs.readFileSync("./test_data/kivakovakivikuva.json")));
+  const instrumentMappings = Array.from(
+    Object.entries(state.instruments),
+    (idAndObj) => [ idAndObj[1].name, { [idAndObj[0].toString()]: "X" } ]
+  );
+  const output = formatAsPage( state, instrumentMappings );
+  expect(output).toMatchSnapshot();
+});
+
+test('notation garlic', () => {
+  const state = createObjects(JSON.parse(fs.readFileSync("./test_data/too_much_garlic.json")));
+  const output = formatAsPage( state, configuredInstrumentMappings );
+  expect(output).toMatchSnapshot();
+});
+
+test('notation kuva', () => {
+  let state = createObjects(JSON.parse(fs.readFileSync("./test_data/kivakovakivikuva.json")));
+  const output = formatAsPage( state, configuredInstrumentMappings );
+  expect(output).toMatchSnapshot();
+});
