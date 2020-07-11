225163306a795676e2bb9abaeb25f7313bb492e0
Andrew Murray <andyranted@gmail.com>
2020-07-11 13:38:35 -0400

    Whole heap of UI work. It's not amazing, but it's alright. Sidebar for pattern navigation, more logic around file loading (for no patterns). Introducing concept of one pattern at a time. Various notes about needing refactors

--- src/App.js
+++ src/App.js
@@ -4,13 +4,43 @@ import Pattern from "./Pattern";
 import h2 from './h2';
 import './App.css';
 
+import { Alert } from '@material-ui/lab';
+
+// define mui theme, including responsiveFont
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+// drawer
+// import { makeStyles } from '@material-ui/core/styles'; // function-style-api
+// import { withStyles } from '@material-ui/styles'; // class-style-api
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import ListItem from '@material-ui/core/ListItem';
+// import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+// mui theme config
+let theme = createMuiTheme( { 
+  palette: { 
+    type: 'dark',
+    primary: { main: '#3f51b5' },
+    secondary: { main: '#f50057' }
+   } 
+} );
+
+
 class App extends React.Component
 {
   constructor(props) {
     super(props);
     this.state = {
       instruments : null,
-      patterns : null
+      patterns : null,
+      selectedPattern : null,
+      loadedFile : null
     };
   }
 
@@ -20,58 +50,128 @@ class App extends React.Component
     h2.parseHydrogenPromise(e.content).then(h => {
       this.setState({
         instruments : h.instruments,
-        patterns : h.patterns
+        patterns : h.patterns,
+        selectedPattern : h.patterns.length === 0 ? null : 0,
+        loadedFile : e.file.name
       });
     });
   }
 
-  render() {
-    if(this.state.patterns == null)
+  selectPattern(patternIndex)
+  {
+    this.setState( { selectedPattern: patternIndex } );
+  }
+
+  // todo: this is a separate component!
+  renderPattern(pattern)
+  {
+    let parts = [];
+    for( const [id, part] of Object.entries(pattern.instrumentTracks))
     {
+      // todo: === expected but doesnt work, unclear how these types dont match
+      const foundElement = this.state.instruments.find( element => (element.id == id) );
+      if( foundElement == null )
+      {
+        continue;
+      }
+      parts.push( [foundElement.name, part] );
+    }
+    return ( 
+      <div>
+        <h1>{pattern.name}</h1>
+        <Pattern 
+          instruments={this.state.instruments} 
+          parts={parts}
+          />
+      </div>
+    );
+  }
+
+  // todo: this will go away eventually, once I choose how to load a file
+  // (though it should obviously be another component anyway)
+  renderMainContent()
+  {
+    if(this.state.patterns == null || this.state.patterns.length === 0)
+    {
+      const showAlert = this.state.patterns != null && this.state.patterns.length === 0;
+      const optionalAlert = showAlert ? ( <Alert severity="error">{this.state.loadedFile} contained no patterns! Try another.</Alert> )
+                                      : "";
       return (
         <div className="App">
-          <header className="App-header">tabit</header>
           <FileImport
             onImport={this.handleFileImport.bind(this)}
             />
+            {optionalAlert}
         </div>
       );      
     }
     else
     {
-      let patternList = [];
-      console.log(this.state.instruments);
-      for( const pattern of this.state.patterns ) {
-        let parts = [];
-        for( const [id, part] of Object.entries(pattern.instrumentTracks))
-        {
-          // todo: === expected but doesnt work, unclear how these types dont match
-          const foundElement = this.state.instruments.find( element => (element.id == id) );
-          if( foundElement == null )
-          {
-            continue;
-          }
-          parts.push( [foundElement.name, part] );
+      let patternContent = null;
+      if( this.state.selectedPattern == null )
+      {
+        patternContent = [];
+        for( const pattern of this.state.patterns ) {
+          patternContent.push( this.renderPattern(pattern) );
         }
-        console.log(parts);
-
-        patternList.push(
-            <div>
-              <h1>{pattern.name}</h1>
-              <Pattern 
-                instruments={this.state.instruments} 
-                parts={parts}
-                />
-            </div>
-        );
+      }
+      else
+      {
+        const patternToRender = this.state.patterns[this.state.selectedPattern];
+        patternContent = this.renderPattern(patternToRender);
       }
 
       return (
         <div className="App">
-          {patternList}
+          {patternContent}
         </div>
-      )
+      );
     }
+  }
+
+  render() {
+    const classes = this.props;
+    const patternsReady = this.state.patterns != null;
+    let mainContent = this.renderMainContent();
+    return (
+      <div className={classes.root}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <AppBar position="fixed" className={classes.appBar}>
+            <Toolbar>
+              <Typography variant="h6" noWrap>
+                tabit
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            variant="persistent"
+            className={classes.drawer}
+            open={patternsReady}
+            classes={{
+              // paper: classes.drawerPaper,
+            }}
+          >
+            <Toolbar />
+            <div className={classes.drawerContainer}>
+              <Typography noWrap={true}>
+              <List>
+                {(this.state.patterns ?? []).map( (pattern, index) => (
+                  <ListItem button key={"drawer-pattern" + index.toString()}>
+                    <ListItemText primary={pattern.name} />
+                  </ListItem>
+                ))}
+              </List>
+              </Typography>
+            </div>
+          </Drawer>
+          <main className={classes.content}>
+            <Toolbar />
+            {mainContent}
+          </main>
+        </ThemeProvider>
+      </div>
+    );
   }
 }
 
