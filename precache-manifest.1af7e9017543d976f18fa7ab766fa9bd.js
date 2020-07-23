e5e50dc3ad5bea53f34979128122dfbaf1f69d39
Andrew Murray <andyranted@gmail.com>
2020-07-22 20:00:28 -0400

    Updates

---
+++ precache-manifest.1af7e9017543d976f18fa7ab766fa9bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "126e384dfd898addef57389064e25c11",
+    "url": "/tabit/index.html"
+  },
+  {
+    "revision": "bac6377b01ca867c355e",
+    "url": "/tabit/static/css/main.c2a7f307.chunk.css"
+  },
+  {
+    "revision": "e758d7f75c6b0afb0e77",
+    "url": "/tabit/static/js/2.985af5cf.chunk.js"
+  },
+  {
+    "revision": "27ca4020711bd21574faae3da7dd14e9",
+    "url": "/tabit/static/js/2.985af5cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bac6377b01ca867c355e",
+    "url": "/tabit/static/js/main.1a32a3c7.chunk.js"
+  },
+  {
+    "revision": "2d87eaf14b46be11aa70",
+    "url": "/tabit/static/js/runtime-main.3ead3703.js"
+  }
+]);
