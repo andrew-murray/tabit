88a24f117477685bb20402c1889f9b755a713cb0
Andrew Murray <andyranted@gmail.com>
2020-08-12 18:23:42 -0400

    Updates

---
+++ precache-manifest.46815310d740fb8cf5edc27d022ebaa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25b36739c8a08806d554b17b0b635af5",
+    "url": "/tabit/index.html"
+  },
+  {
+    "revision": "e422dd61e207d44538ad",
+    "url": "/tabit/static/css/main.abb25dff.chunk.css"
+  },
+  {
+    "revision": "d93921fc732b519cdfb5",
+    "url": "/tabit/static/js/2.93dc6165.chunk.js"
+  },
+  {
+    "revision": "27ca4020711bd21574faae3da7dd14e9",
+    "url": "/tabit/static/js/2.93dc6165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e422dd61e207d44538ad",
+    "url": "/tabit/static/js/main.ef7d0b77.chunk.js"
+  },
+  {
+    "revision": "2d87eaf14b46be11aa70",
+    "url": "/tabit/static/js/runtime-main.3ead3703.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tabit/precache-manifest.a2f855f8b5175668acfbe0cb4e442754.js"
+  "/tabit/precache-manifest.46815310d740fb8cf5edc27d022ebaa3.js"
 );
 
 self.addEventListener('message', (event) => {
