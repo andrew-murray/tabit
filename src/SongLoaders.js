00e727a31a02b19bdaeb16ee6c9758ec4f2de7dd
Andrew Murray <andyated@gmail.com>
2022-02-14 16:03:01 -0500

    Update comment about hydrogen volumes

--- src/SongLoaders.js
+++ src/SongLoaders.js
@@ -69,10 +69,10 @@ function createPatternsFromData(patternData)
 
 function prepHydrogenVolumes(instrumentIndex)
 {
-  // fixme: convert hydrogen volume/gain to normal values, somewhere
   for( let instrument of instrumentIndex )
   {
-    // hydrogen's volume sliders seem to be a linear slider between [0,1.5]
+    // hydrogen's volume sliders seem to be a slider between [0,1.5]
+    // (let's land it on our volume-curve and hope for the best)
     const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
     instrument.volume = clamp(instrument.volume / 1.5, 0.0, 1.5);
   }
