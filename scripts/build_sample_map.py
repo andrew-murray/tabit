391a4c28b208fe88e3e97b2b2c74244231e2dddd
Andrew Murray <andyated@gmail.com>
2022-05-05 13:07:11 -0400

    Utilities to help build lists of samples (to use in APIs).

---
+++ scripts/build_sample_map.py
@@ -0,0 +1,20 @@
+import os, sys
+import json
+import glob
+
+
+def build_sample_map(input_dir, output_file):
+    is_audio = lambda f : f.endswith(".wav") or f.endswith(".mp3") or f.endswith(".flac")
+    samples = {
+        x : [f for f in os.listdir(os.path.join(input_dir, x)) if is_audio(f)] for x in os.listdir(input_dir)
+    }
+    json.dump(
+        samples,
+        open(output_file, 'w'),
+        indent = 4,
+        sort_keys=True
+    )
+
+
+if __name__ == "__main__":
+    build_sample_map("./public/wav/", "samples.json")
