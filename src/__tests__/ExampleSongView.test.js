014d17d09ef8c0d0a84f38a65cd9633c03bd34dd
Andrew Murray <andyated@gmail.com>
2022-06-07 14:01:54 -0400

    Amend test, to deal with the async nature of how loading works.

--- src/__tests__/ExampleSongView.test.js
+++ src/__tests__/ExampleSongView.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render} from '@testing-library/react';
+import {render, waitFor} from '@testing-library/react';
 import {ExampleSongView} from '../LazySongViews';
 import { MemoryRouter } from 'react-router-dom'
 
@@ -15,11 +15,23 @@ function AppWithinRouter()
   );
 };
 
-test('ExampleSongView loads and renders title', () => {
-  const { getAllByText } = render(<AppWithinRouter />);
+test('ExampleSongView loads and renders title', async () => {
+  const component = render(<AppWithinRouter />);
 
   // note, there are multiple elements that say "tabit"
   // now but this test mostly tests the App.js loads without complaint
-  const titleElement = getAllByText(/kuva/i)[0];
-  expect(titleElement).toBeInTheDocument();
+  const progressElement = component.getAllByText(/Loading song/i)[0];
+  expect(progressElement).toBeInTheDocument();
+  const assertOnTitleComponent = () => {
+    const renderedTitleElements = component.getAllByText(/KUVA/i)
+    if(renderedTitleElements.length === 0)
+    {
+      return false;
+    }
+    else
+    {
+      return expect(renderedTitleElements[0]).toBeInTheDocument();
+    }
+  };
+  await waitFor(assertOnTitleComponent);
 });
