96c5b02522fb62224c89afbc80da4192906d0ed4
Andrew Murray <andyated@gmail.com>
2023-03-20 14:34:22 -0400

    fix bug where h2 file with single pattern wouldn't be parsed correctly

--- src/h2.js
+++ src/h2.js
@@ -17,13 +17,9 @@ function calculatePatternResolution(pattern, size)
 
 function parseHydrogen(dom, sparse)
 {
-  // fixme:
-  // this parsing often assumes there's >=2 elements
-  // note: this code has undergone a significant refactoring, as port of changing
-  // the underlying parser, this fixme may no longer apply
-
-  // this "zero" here is presumably an artefact of xml --> json representation
-  const instrumentElements = dom.song.instrumentList.instrument;
+  // deal with single elements (not wrapped in an array) or multiple
+  // elements (which appears as an array)
+  const instrumentElements = [].concat(dom.song.instrumentList.instrument);
 
   // [  { id, name } ]
   const instrumentArray = Array.from(
@@ -67,7 +63,9 @@ function parseHydrogen(dom, sparse)
   );
 
 
-  const patternElements = dom.song.patternList.pattern;
+  // deal with single elements (not wrapped in an array) or multiple
+  // elements (which appears as an array)
+  const patternElements = [].concat(dom.song.patternList.pattern);
   // patterns
   // [  { name, size, notes } ]
   const patternArray = Array.from(
@@ -142,123 +140,122 @@ function parseHydrogen(dom, sparse)
   );
 
   // todo: refactor into (at least one) separate function
-  if(dom.song.virtualPatternList)
+  if(dom.song.virtualPatternList && dom.song.virtualPatternList.pattern)
   {
     // so unfortunately, virtualPatternGroup represents a directional graph and we have to build
     // the tree of dependencies for each node, we implement this in a very simplistic way
     // let's build a mapping( name -> [ names ] ) and continue to resolve it
     // until we're done
-    const virtualPatternGroups = dom.song.virtualPatternList.pattern;
-    if( virtualPatternGroups )
+
+    // deal with single elements (not wrapped in an array) or multiple
+    // elements (which appears as an array)
+    const virtualPatternGroups = [].concat(dom.song.virtualPatternList.pattern);
+    // each element looks like
+    //
+    // <pattern>
+    // <name>p2-a-djembe</name>
+    // <virtual>p2-a-bass</virtual>
+    // <virtual>p2-snare</virtual>
+    // </pattern>
+
+    let patternToRelated = {};
+
+    // record initial relations
+    for( const virtualGroup of Array.from(virtualPatternGroups) )
     {
-      // each element looks like
-      //
-      // <pattern>
-      // <name>p2-a-djembe</name>
-      // <virtual>p2-a-bass</virtual>
-      // <virtual>p2-snare</virtual>
-      // </pattern>
-
-      let patternToRelated = {};
-
-      // record initial relations
-      for( const virtualGroup of Array.from(virtualPatternGroups) )
+      const rootPatternName = virtualGroup.name;
+      if( typeof virtualGroup.virtual === 'string')
       {
-        const rootPatternName = virtualGroup.name;
-        if( typeof virtualGroup.virtual === 'string')
-        {
-          patternToRelated[rootPatternName] = new Set([virtualGroup.virtual]);
-        }
-        else
-        {
-          patternToRelated[rootPatternName] = new Set(Array.from(virtualGroup.virtual));
-        }
+        patternToRelated[rootPatternName] = new Set([virtualGroup.virtual]);
       }
-
-      // expand connections until our object stops changing, brute-force
-      // this is a relatively large limit but is better than the potential of an infinite loop
-      // I think 3 layers would be pushing this feature
-      const MAX_ITERATIONS = 20;
-      for(let iteration = 0; iteration < MAX_ITERATIONS; ++iteration)
+      else
       {
-        let expandedObject = {};
-        // we could do a check at the end of each loop, but it's easier to track object equality this way
-        let objectHasExpanded = false;
-        for(const [root, related] of Object.entries(patternToRelated))
+        patternToRelated[rootPatternName] = new Set(Array.from(virtualGroup.virtual));
+      }
+    }
+
+    // expand connections until our object stops changing, brute-force
+    // this is a relatively large limit but is better than the potential of an infinite loop
+    // I think 3 layers would be pushing this feature
+    const MAX_ITERATIONS = 20;
+    for(let iteration = 0; iteration < MAX_ITERATIONS; ++iteration)
+    {
+      let expandedObject = {};
+      // we could do a check at the end of each loop, but it's easier to track object equality this way
+      let objectHasExpanded = false;
+      for(const [root, related] of Object.entries(patternToRelated))
+      {
+        let expandedNodeSet = new Set(related);
+        for( const node of expandedNodeSet )
         {
-          let expandedNodeSet = new Set(related);
-          for( const node of expandedNodeSet )
+          if( node in patternToRelated )
           {
-            if( node in patternToRelated )
-            {
-              // set union
-              expandedNodeSet = new Set([...expandedNodeSet, ...patternToRelated[node]]);
-            }
+            // set union
+            expandedNodeSet = new Set([...expandedNodeSet, ...patternToRelated[node]]);
           }
-          objectHasExpanded = objectHasExpanded || ( expandedNodeSet.size !== related.size );
-          expandedObject[ root ] = expandedNodeSet;
+        }
+        objectHasExpanded = objectHasExpanded || ( expandedNodeSet.size !== related.size );
+        expandedObject[ root ] = expandedNodeSet;
 
-        }
-        // exit if no change
-        if(!objectHasExpanded)
-        {
-          break;
-        }
-        // throw if we've failed to resolve all the connections by now, morelikely something has gone wrong
-        if( iteration === MAX_ITERATIONS )
-        {
-          throw new Error("Reached max virtual_pattern recursion depth.");
-        }
-        // otherwise update mapping and continue
-        patternToRelated = expandedObject;
       }
-
-      for( const [rootPatternName, relatedPatternSet] of Object.entries(patternToRelated) )
+      // exit if no change
+      if(!objectHasExpanded)
       {
-        // could do filter, and assert on length?
-        let rootPattern = patternsWithTracks.find(p => p.name === rootPatternName);
-        for( const patternToMergeName of relatedPatternSet )
+        break;
+      }
+      // throw if we've failed to resolve all the connections by now, morelikely something has gone wrong
+      if( iteration === MAX_ITERATIONS )
+      {
+        throw new Error("Reached max virtual_pattern recursion depth.");
+      }
+      // otherwise update mapping and continue
+      patternToRelated = expandedObject;
+    }
+
+    for( const [rootPatternName, relatedPatternSet] of Object.entries(patternToRelated) )
+    {
+      // could do filter, and assert on length?
+      let rootPattern = patternsWithTracks.find(p => p.name === rootPatternName);
+      for( const patternToMergeName of relatedPatternSet )
+      {
+        const patternToMerge = patternsWithTracks.find(p => p.name === patternToMergeName );
+        for( const [id, t] of Object.entries(patternToMerge.instrumentTracks) )
         {
-          const patternToMerge = patternsWithTracks.find(p => p.name === patternToMergeName );
-          for( const [id, t] of Object.entries(patternToMerge.instrumentTracks) )
+          if( id in rootPattern.instrumentTracks )
           {
-            if( id in rootPattern.instrumentTracks )
+            // we match hydrogen's implementation here and discard values past the length of the original track
+            const expand = false;
+            const merged = rootPattern.instrumentTracks[ id ].aggregate( t, expand);
+            rootPattern.instrumentTracks[ id ] = merged;
+          }
+          else
+          {
+            if(sparse)
             {
-              // we match hydrogen's implementation here and discard values past the length of the original track
-              const expand = false;
-              const merged = rootPattern.instrumentTracks[ id ].aggregate( t, expand);
-              rootPattern.instrumentTracks[ id ] = merged;
+              rootPattern.instrumentTracks[id] = t.shrinkTo(rootPattern.size);
             }
             else
             {
-              if(sparse)
-              {
-                rootPattern.instrumentTracks[id] = t.shrinkTo(rootPattern.size);
-              }
-              else
-              {
-                // ensure track is the appropriate length & res
-                let copiedTrack = t.format(rootPattern.resolution);
-                copiedTrack.length = rootPattern.size  / rootPattern.resolution;
-                rootPattern.instrumentTracks[id] = copiedTrack;
-              }
+              // ensure track is the appropriate length & res
+              let copiedTrack = t.format(rootPattern.resolution);
+              copiedTrack.length = rootPattern.size  / rootPattern.resolution;
+              rootPattern.instrumentTracks[id] = copiedTrack;
             }
           }
         }
-        // reassess resolution and apply to all tracks
-        // this may not be necessary (even when doing dense tracks) but it's probably nice
-        if(!sparse)
+      }
+      // reassess resolution and apply to all tracks
+      // this may not be necessary (even when doing dense tracks) but it's probably nice
+      if(!sparse)
+      {
+        const resolution = calculatePatternResolution(rootPattern, rootPattern.size);
+        rootPattern.resolution = resolution;
+        for( const [id, track] of Object.entries(rootPattern.instrumentTracks) )
         {
-          const resolution = calculatePatternResolution(rootPattern, rootPattern.size);
-          rootPattern.resolution = resolution;
-          for( const [id, track] of Object.entries(rootPattern.instrumentTracks) )
-          {
-            // ensure that
-            rootPattern[id] = track.format( resolution );
-          }
+          // ensure that
+          rootPattern[id] = track.format( resolution );
         }
       }
-
     }
   }
 
