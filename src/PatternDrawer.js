78f1eb8c513d4ad019dd5b4b1af4d4a94cf79908
Andrew Murray <andyated@gmail.com>
2024-01-13 23:55:45 +0000

    Add dnd-kit and a prototype dragging implementation on the pattern-drawer

--- src/PatternDrawer.js
+++ src/PatternDrawer.js
@@ -9,7 +9,20 @@ import TabitBar from "./TabitBar";
 import ClearIcon from '@mui/icons-material/Clear';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import { isMobile } from "./Mobile";
+import DragHandleIcon from '@mui/icons-material/DragHandle';
 import Tooltip from "./TabitTooltip";
+import {arrayMove, useSortable, SortableContext, sortableKeyboardCoordinates, 
+  verticalListSortingStrategy} from '@dnd-kit/sortable';
+import {CSS} from '@dnd-kit/utilities';
+import { 
+  DragOverlay,
+  DndContext, 
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors
+} from '@dnd-kit/core';
 
 const PatternListItem = (props) =>
 {
@@ -33,12 +46,31 @@ const PatternListItem = (props) =>
     },
     [index, onRemove]
   );
-  return (<ListItem
+  return (
+    <ListItem
     button
     key={"drawer-pattern" + index.toString()}
     onClick={selectCallback}
   >
     <ListItemText primary={pattern.name} />
+    {
+      <ListItemSecondaryAction
+        {...props.dragListeners}
+        {...props.dragAttributes}
+      >
+        <Tooltip
+          title="Drag"
+          show={props.showHelp}
+        >
+          <IconButton
+            edge="end"
+            size="small"
+          >
+            <DragHandleIcon fontSize="small"/>
+          </IconButton>
+        </Tooltip>
+      </ListItemSecondaryAction>
+    }
     {onRemove &&
       <ListItemSecondaryAction>
         <Tooltip
@@ -55,28 +87,103 @@ const PatternListItem = (props) =>
         </Tooltip>
       </ListItemSecondaryAction>
     }
-  </ListItem>
+    </ListItem>
+  );
+};
+
+const DraggablePatternListItem = (props) =>
+{
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition
+  } = useSortable({id: props.id});
+  
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition
+  };
+  return (
+    <div
+      ref={setNodeRef}
+      style={style}
+    >
+      <PatternListItem 
+        selectPattern={props.selectPattern}
+        index={props.index}
+        pattern={props.pattern}
+        onRemove={props.onRemove}
+        dragListeners={listeners}
+        dragAttributes={attributes}
+      />
+    </div>
   );
 };
 
 function DrawerContent(props)
 {
+  const [activeId, setActiveId] = React.useState(null);
+  const [items,setItems] = React.useState( props.patterns ?? [] );
+  const sensors = useSensors(
+    useSensor(PointerSensor)
+  );
+
+  function handleDragEnd(event) {
+    console.log(event)
+    const {active, over} = event;
+    const itemNames = items.map(item => item.name);
+    if (active.id !== over.id) {
+      const oldIndex = itemNames.indexOf(active.id);
+      const newIndex = itemNames.indexOf(over.id);
+      setItems((items) => {        
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  };
+  function handleDragStart(event) {
+    const {active} = event;
+    setActiveId(active.id);
+  };
+  const nameAndIndexArray = [...items.keys()].map(index => [items[index].name, index]);
+  const patternNameToIndex = new Map (nameAndIndexArray);
   return (<React.Fragment>
     {!isMobile ? <TabitBar placeholder /> : null }
     <div
       style={{overflow: "auto"}}
     >
-      <List>
-        {(props.patterns ?? []).map( (pattern, index) =>
-          <PatternListItem
-            pattern={pattern}
-            key={"pattern-key-" + String(index)}
-            index={index}
-            onRemove={props.onRemove}
-            selectPattern={props.selectPattern}
-            showHelp={props.showHelp}
-          />
-        )}
+      <div>
+        <DndContext
+          sensors={sensors}
+          collisionDetection={closestCenter}
+          onDragEnd={handleDragEnd}
+          onDragStart={handleDragStart}
+        >
+          <SortableContext items={items} strategy={verticalListSortingStrategy}>
+            {(items).map( (pattern, index) =>
+              <DraggablePatternListItem
+                pattern={pattern}
+                key={pattern.name}
+                id={pattern.name}
+                index={index}
+                onRemove={props.onRemove}
+                selectPattern={props.selectPattern}
+                showHelp={props.showHelp}
+              />
+            )}
+          </SortableContext>
+          <DragOverlay>
+            {activeId && 
+              <PatternListItem 
+                selectPattern={props.selectPattern}
+                index={patternNameToIndex.get(activeId)}
+                pattern={items[patternNameToIndex.get(activeId)]}
+                onRemove={props.onRemove}
+              />
+            }
+          </DragOverlay>
+        </DndContext>
         {props.onAdd &&
           <ListItem
             key={"drawer-add-button"}
@@ -102,13 +209,13 @@ function DrawerContent(props)
             </ListItemSecondaryAction>
           </ListItem>
         }
-      </List>
+      </div>
     </div>
   </React.Fragment>
   );
 }
 
-const MemoizedDrawerContent = React.memo(DrawerContent);
+const MemoizedDrawerContent = DrawerContent; // React.memo(DrawerContent);
 
 function PatternDrawer(props)
 {
@@ -133,6 +240,7 @@ function PatternDrawer(props)
         selectPattern={props.selectPattern}
         onAdd={props.onAdd}
         showHelp={props.showHelp}
+        onReorderPatterns={props.onReorderPatterns}
       />
     </SwipeableDrawer>
   );
