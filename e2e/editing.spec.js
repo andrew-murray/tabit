de40c6796cb7ecead47729a76c5a18c90499b27d
Andrew Murray <andyated@gmail.com>
2026-04-26 11:40:41 +0200

    test: start building a playwright test-suite

---
+++ e2e/editing.spec.js
@@ -0,0 +1,6 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+
+test.describe("Editing", () => {
+  // Tests for editing behavior (and locked vs unlocked behaviour) go here
+});
